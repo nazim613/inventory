93635b5ffc9a7edeed322cd15d2b3ee2cfa05b89
dharweshsolutions-ship-it <syeddharu31@gmail.com>
2026-03-16 14:34:36 +0530

    base url changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://inventory-4r4g.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
